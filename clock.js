3d5dbad6037a33d3fa80d104847423f0cffea25a
shreyamishra1609 <71901526+shreyamishra1609@users.noreply.github.com>
2020-09-26 16:39:34 +0530

    Delete clock.js

--- clock.js
+++
@@ -1,8 +0,0 @@
-<script type="text/javascript">
-var clock=setInterval(clocktiming,1000)
-function clocktiming(){
-var date= Date();
-var shreya=date.toLocaleTimeString();
-document.getElementById("start").innerHTML=shreya;
-}
-</script>
